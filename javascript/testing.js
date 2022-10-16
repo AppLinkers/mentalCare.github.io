400642ffbd500461ded6ec51d9f8a4b10e3cb13f
rio319-an <78468001+rio319-an@users.noreply.github.com>
2022-10-16 16:28:39 +0900

    testing js

--- javascript/testing.js
+++ javascript/testing.js
@@ -1,10 +1,28 @@
-const detailBoxs = document.querySelectorAll('.detail__box');
-const detailInside = document.querySelector('.detail__inside');
+// const detailBoxs = document.querySelectorAll('.detail__box');
+// const detailInside = document.querySelector('.detail__inside');
 
-// 수정 필요
-detailBoxs.forEach(detailBox => {
-    detailBox.addEventListener("click", ()=> {
-        detailBox.classList.toggle('active');
-        detailInside.classList.toggle('active');
-    })
-})
+// // 수정 필요
+// detailBoxs.forEach(detailBox => {
+//     detailBox.addEventListener("click", () => {
+//         detailBox.classList.toggle('active');
+//         detailInside.classList.toggle('active');
+//     })
+// })
+
+const inputId = ["worst", "high", "soso", "low", "none"];
+
+const groups = document.querySelectorAll(".form-group");
+
+for (var i = 0; i < groups.length; i++) {
+    const group = groups[i];
+    const checks = group.querySelectorAll(".check");
+    for (var j = 0; j < 5; j++) {
+        const check = checks[j];
+        const input = check.querySelector("input");
+        const label = check.querySelector("label");
+
+        input.name = "check" + i;
+        input.id = inputId[j] + i;
+        label.htmlFor = inputId[j] + i;
+    }
+}
