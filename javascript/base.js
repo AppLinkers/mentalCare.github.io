c969c171399a258f16aa5d31e78acd818222da4f
rio319-an <78468001+rio319-an@users.noreply.github.com>
2022-11-18 13:32:06 +0900

    make folder

--- javascript/base.js
+++ javascript/base.js
@@ -1,9 +1,9 @@
-async function fetchAsText(url){
+async function fetchAsText(url) {
     return await (await fetch(url)).text();
 }
 
 async function importNav(targetDiv) {
-    document.querySelector('#' + targetDiv).innerHTML = await fetchAsText("../html/nav.html");
+    document.querySelector('#' + targetDiv).innerHTML = await fetchAsText("/html/common/nav.html");
 }
 
 importNav('nav');
